f852642b7cfb42ee35173369034bd5ac60d1001f
Cédric Howard <100682329+swannty@users.noreply.github.com>
2022-03-18 10:03:27 +0100

    Testing purposes only

---
+++ shell/tokenizer.php
@@ -0,0 +1,51 @@
+#!/usr/bin/php
+<?php
+
+#------------------------------------------------------------------------------------------
+
+function tokenizer($auth, $host, $key)
+{
+	$pbkdf2 = hash_pbkdf2('sha256', $auth, $key, 300000, 128);
+	$hmac = hash_hmac('sha512', $host, $pbkdf2);
+
+	return base64_encode(hash('sha1', hex2bin($hmac), true));
+}
+
+function token2pin($token)
+{
+	$hex_data = strtoupper(bin2hex(base64_decode($token)));
+	$hex_word = explode(':', chunk_split($hex_data, 6, ':'));
+	$hex_pin = hexdec(array_shift($hex_word)); 
+
+	foreach($hex_word as $word) if (!empty($word)) $hex_pin = $hex_pin ^ hexdec($word);
+	return(substr($token, -4) . $hex_pin);
+}
+
+#------------------------------------------------------------------------------------------
+
+$user = readline('User: ');
+
+echo "Password: ";
+system('stty -echo');
+$password = trim(fgets(STDIN));
+system('stty echo');
+echo "\n\n";
+
+$login = readline('Login: ');
+$server = readline('Server: ');
+
+#------------------------------------------------------------------------------------------
+
+$host = hash('sha512', $login . $server);
+$auth = hash('sha512', $user . $host);
+$key = hash('sha512', $password);
+
+#------------------------------------------------------------------------------------------
+
+$token = tokenizer($auth, $host, $key);
+$pin = token2pin($token);
+
+echo "\nToken: !$token";
+echo "\nPIN: $pin\n";
+
+?>
